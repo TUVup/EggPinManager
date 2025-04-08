6f83db9df38eff7c3b48bd3b1cc705cd691a9bd9
TUVup <72339032+TUVup@users.noreply.github.com>
2025-04-08 13:38:43 +0900

    up to 1.2.0

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from cx_Freeze import setup, Executable
-versions = '1.1.1'
+versions = '1.2.6'
 
 build_exe_options = {
+    "packages": ["comtypes"],
     "include_files": ['resource/'],
-    "build_exe": f"EggManager_{versions}_cx"
+    "build_exe": f"EggManager_{versions}"
 }
  
-exe = [Executable(f'EggManager_GUI_{versions}_cx.py', base='gui', target_name=f'EggManager_{versions}', icon='eggui', uac_admin=True)]
+exe = [Executable(f'EggManager_GUI.py', base='gui', target_name=f'EggManager_{versions}', icon='resource/eggui', uac_admin=True)]
  
 setup(
     name='EggManager',
